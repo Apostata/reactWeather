ac09a5cba67a14b0deb7178a69fd51ee19f95fba
Apostata <rene.souza@gmail.com>
2017-03-20 23:02:36 -0300

    update middleware for localhost

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-	if(req.headers['x-forwarded-proto'] ==='http'){
-		next();
-	}
-	else{
+
+	if(req.headers['x-forwarded-proto'] ==='https'){ //verifica se o protocolo é http
 		res.redirect('http://'+req.hostname + req.url);
+	}
+	else{//se o protocolo é https, redireciona para http
+		next();
 	}
 });
 
