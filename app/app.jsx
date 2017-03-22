870b77de9d07aba9fbae86b39b15956a6183cd16
Apostata <rene.souza@gmail.com>
2017-03-21 22:45:01 -0300

    Add foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ import Weather from 'Weather';
 import About from 'About';
 import Examples from 'Examples';
 
+//load foundation
+import 'style!css!foundation-sites/dist/foundation.min.css';
+jQuery(document).foundation();
+
 ReactDOM.render(//se não encontrar about e nem examples então ele carrega weather que é o index
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 var webpack = require('webpack');
+
 module.exports = {
-	entry: './app/app.jsx',
+	entry: [
+		'script!jquery/dist/jquery.min.js',
+		'script!foundation-sites/dist/foundation.min.js',
+		'./app/app.jsx',
+	],
+
+	externals:{
+		jquery:'jQuery'
+	},
 
 	output: {
 	    path: __dirname,
@@ -34,14 +43,18 @@ module.exports = {
 	      }
 	    ]
   	},
-  	devtool: 'cheap-module-eval-source-map'
+  	devtool: 'cheap-module-eval-source-map',
 
-	/*plugins: [
+	plugins: [
 	  // Minify assets.
-		new webpack.optimize.UglifyJsPlugin({
+		/*new webpack.optimize.UglifyJsPlugin({
 		    compress: {
 		      warnings: false // https://github.com/webpack/webpack/issues/1496
 		    }
+		})*/
+		new webpack.ProvidePlugin({
+			"$": 'jqeury',
+			"jQuery": 'jquery'
 		})
-	]*/
+	]
 };
